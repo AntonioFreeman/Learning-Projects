3c6025a7cc557d685b75b565c611e01453308dd6
AntonioFreeman <chulyuchkin@yandex.ru>
2019-01-14 14:10:22 +0400

    Commit1

---
+++ Tester/Tester/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester
+{
+    public class Question : IEnumerable
+    {
+        private string textQuestion;
+        private List<string> answers;
+
+
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tester/Tester/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester
+{
+    public class Test : INotifyPropertyChanged
+    {
+        private string title;
+        private int amountQuestions;
+        private List<Question> questions;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
